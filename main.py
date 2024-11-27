c1bed76048f994d0bd61415554b24a66d80d9479
naivash <n.ivaschenko@facct.ru>
2024-11-28 01:00:31 +0300

    add cors rule

--- main.py
+++ main.py
@@ -1,9 +1,18 @@
 import uvicorn
 from fastapi import FastAPI
-
 from api.router import router
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
 app.include_router(
     router,
     prefix="/api"
